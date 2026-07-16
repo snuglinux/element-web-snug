95e9f15e9f85fe7dd947c6cd0560d65e3cb482e8
Pro7777tim <khtim1226@gmail.com>
2026-07-17 00:00:21 +0300

    Fix UIFeature.allowPrivateRooms bug

--- apps/web/src/viewmodels/room-list/RoomListHeaderViewModel.ts
+++ apps/web/src/viewmodels/room-list/RoomListHeaderViewModel.ts
@@ -28,7 +28,6 @@ import {
 import type { ViewRoomPayload } from "../../dispatcher/payloads/ViewRoomPayload";
 import type { RoomListSectionsCollapseStateChangedPayload } from "../../dispatcher/payloads/RoomListSectionsCollapseStateChangedPayload";
 import SettingsStore from "../../settings/SettingsStore";
-import { useSettingValue } from "../../hooks/useSettings";
 import { UIFeature } from "../../settings/UIFeature.ts";
 import RoomListStoreV3 from "../../stores/room-list-v3/RoomListStoreV3";
 import { SortingAlgorithm } from "../../stores/room-list-v3/skip-list/sorters";
@@ -333,8 +332,8 @@ function computeHeaderSpaceState(
     const activeSpace = spaceStore.activeSpaceRoom;
     const title = getHeaderTitle(spaceStore);
 
-    const allowPublicRooms = useSettingValue(UIFeature.AllowCreatingPublicRooms);
-    const allowPrivateRooms = useSettingValue(UIFeature.AllowCreatingPrivateRooms);
+    const allowPublicRooms = SettingsStore.getValue(UIFeature.AllowCreatingPublicRooms);
+    const allowPrivateRooms = SettingsStore.getValue(UIFeature.AllowCreatingPrivateRooms);
 
     const canCreateRoom = hasCreateRoomRights(matrixClient, activeSpace) && (allowPublicRooms || allowPrivateRooms);
     const canCreateVideoRoom = getCanCreateVideoRoom(canCreateRoom);
